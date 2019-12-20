cc8ff0beff1258927a0d5ffc90a040cdf057f954
Prakhar Agarwal <prakhargvp@gmail.com>
2019-12-20 16:13:38 +0530

    Introduction to Mutation, NonNull Fields and Mutation

--- schema/schema.js
+++ schema/schema.js
@@ -6,7 +6,8 @@ const {
 	GraphQLString,
 	GraphQLInt,
 	GraphQLSchema,
-	GraphQLList
+	GraphQLList,
+	GraphQLNonNull
 } = graphql;
 
 const CompanyType = new GraphQLObjectType({
@@ -41,6 +42,48 @@ const UserType = new GraphQLObjectType({
 	})
 });
 
+const mutation = new GraphQLObjectType({
+	name: 'Mutation',
+	fields: {
+		addUser: {
+			type: UserType,
+			args: {
+				firstName: { type: new GraphQLNonNull(GraphQLString) },
+				age: { type: new GraphQLNonNull(GraphQLInt) },
+				companyId: { type: GraphQLString }
+			},
+			resolve(parentValue, { firstName, age }) {
+				return axios.post('http://localhost:3000/users', {
+					firstName, age
+				}).then(res => res.data);
+			}
+		},
+		deleteUser: {
+			type: UserType,
+			args: {
+				id: { type: new GraphQLNonNull(GraphQLString) }
+			},
+			resolve(parentValue, { id }) {
+				return axios.delete(`http://localhost:3000/users/${id}`)
+					.then(res => res.data);
+			}
+		},
+		editUser: {
+			type: UserType,
+			args: {
+				id: { type: new GraphQLNonNull(GraphQLString) },
+				firstName: { type: GraphQLString },
+				age: { type: GraphQLInt },
+				companyId: { type: GraphQLString }
+			},
+			resolve(parentValue, args) {
+				return axios.patch(`http://localhost:3000/users/${args.id}`, args)
+  					.then(res => res.data);
+
+			}
+		}
+	}
+});
 
 const RootQuery = new GraphQLObjectType({
 	name: 'RootQueryType',
@@ -65,5 +108,6 @@ const RootQuery = new GraphQLObjectType({
 });
 
 module.exports = new GraphQLSchema({
-	query: RootQuery
+	query: RootQuery,
+	mutation
 })
