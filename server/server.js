9db108effcfc2db39be3b02f99276d5a312c7b2e
Prakhar Agarwal <prakhargvp@gmail.com>
2020-01-09 05:07:56 +0530

    Adding Signin, Logout Mutation

--- server/server.js
+++ server/server.js
@@ -12,14 +12,16 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+// 
+
+const MONGO_URI = 'mongodb://admin:dZImophFGXRXOkEm@cluster0-shard-00-00-907ld.mongodb.net:27017,cluster0-shard-00-01-907ld.mongodb.net:27017,cluster0-shard-00-02-907ld.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority';
 
 // Mongoose's built in promise library is deprecated, replace it with ES2015 Promise
 mongoose.Promise = global.Promise;
 
 // Connect to the mongoDB instance and log a message
 // on success or failure
-mongoose.connect(MONGO_URI);
+mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.connection
     .once('open', () => console.log('Connected to MongoLab instance.'))
     .on('error', error => console.log('Error connecting to MongoLab:', error));
